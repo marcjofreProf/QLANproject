dfeb2aad87a7d374c070cdec71b0d85fdfa7ff48
Marc Jofre <marcjofre@OPTIPLEX-Ubuntu-VM.myguest.virtualbox.org>
2023-12-28 18:47:30 +0100

    Adding Jupyter and Python files before swig

--- JupyterPythonScripts/QsessionLayerAgent.py
+++ JupyterPythonScripts/QsessionLayerAgent.py
@@ -4,5 +4,7 @@
 #####################################################
 
 import os, sys
+pathScriptBelowAgentScript='../CppScripts/'
+sys.path.append(pathScriptBelowAgentScript)
 
 import QtransportLayerAgent
