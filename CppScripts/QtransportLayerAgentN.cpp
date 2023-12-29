1c6698790459bcd7e011f30e92476191f430d7bd
Marc Jofre <marcjofre@OPTIPLEX-Ubuntu-VM.myguest.virtualbox.org>
2023-12-29 12:21:36 +0100

    Initial agent transport N

--- CppScripts/QtransportLayerAgentN.cpp
+++ CppScripts/QtransportLayerAgentN.cpp
@@ -21,7 +21,7 @@ QTLAN::~QTLAN() {
 } /* namespace nsQnetworkLayerAgentN */
 
 
-// For testing
+// For running in the quantum node
 
 using namespace nsQtransportLayerAgentN;
 
@@ -48,6 +48,43 @@ int main(int argc, char const * argv[]){
  // }
  //}
  
- return 0;
+ QTLAN QTLANagent(0); // Initiate the instance with 0 sessions connected.
+ QTLANagent.pause(); // Initiate in paused state.
+ cout << "Starting in pause state the QtransportLayerAgentN" << endl;
+ bool isValidWhileLoop = true;
+ 
+ while(isValidWhileLoop){
+       switch(QTLANagent.getState()) {
+           case QTLAN::APPLICATION_RUNNING: {
+               
+               // Do Some Work
+               QTLANagent.pause(); // After procesing the request, pass to paused state
+               break;
+           }
+           case QTLAN::APPLICATION_PAUSED: {
+               // Wait Till You Have Focus Or Continues
+               if (true){
+               	QTLANagent.start();
+               }
+               else if (false){
+               	QTLANagent.exit();// Change The Application State to exit
+               }
+               else{               
+	        QTLANagent.pause(); // Keep paused state
+               }
+               break;
+           }
+           case QTLAN::APPLICATION_EXIT: {    
+               cout << "Exiting the QtransportLayerAgentN" << endl;
+               isValidWhileLoop=false;//break;
+           }
+           default: {
+               // ErrorHandling Throw An Exception Etc.
+           }
+
+        } // switch
+    }
+   
+ return 0; // Everything Ok
 }
 
--- CppScripts/QtransportLayerAgentN.h
+++ CppScripts/QtransportLayerAgentN.h
@@ -19,12 +19,32 @@ namespace nsQtransportLayerAgentN {
 // enum GPIO_DIRECTION{ INPUT, OUTPUT };
 
 class QTLAN {
+public:
+	enum ApplicationState {
+		APPLICATION_RUNNING = 0,
+		APPLICATION_PAUSED = 1,  // Out of Focus or Paused If In A Timed Situation
+		APPLICATION_EXIT = -1,
+	    };
+
 private:
 	int numberSessions;
-
+	// Member Variables Such As Window Handle, Time Etc.,
+	ApplicationState m_state;
+	
 public:
 	QTLAN(int numberSessions); //constructor
-	
+	// virtual ~Application(); // Default Okay - Use Virtual If Using Inheritance
+        ApplicationState getState() const { return m_state; }
+
+        bool start() { m_state = APPLICATION_RUNNING; return true; }
+        bool pause() { m_state = APPLICATION_PAUSED; return true; } 
+        // resume may keep track of time if the application uses a timer.
+        // This is what makes it different than start() where the timer
+        // in start() would be initialized to 0. And the last time before
+        // paused was trigger would be saved, and then reset as new starting
+        // time for your timer or counter. 
+        bool resume() { m_state = APPLICATION_RUNNING; return true; }      
+        bool exit() { m_state = APPLICATION_EXIT;  return false; }
 	virtual ~QTLAN();  //destructor
 
 //private:
