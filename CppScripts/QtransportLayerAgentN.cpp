85b1871d7ea4f4e90d65a271b6156dd46885e93a
marcjofre <marcjofre@VM-Ubuntu-Tower3420.myguest.virtualbox.org>
2024-01-04 22:15:26 +0100

    debugging 2

--- CppScripts/QtransportLayerAgentN.cpp
+++ CppScripts/QtransportLayerAgentN.cpp
@@ -268,7 +268,9 @@ strcpy(this->SendBuffer,ParamsCharArray);//strtok(NULL,","));
     //cout << "IPaddressesSockets: " << IPaddressesSockets << endl;
     // Understand which socket descriptor has to be used
     int socket_fd_conn;
+    cout << "IPaddressesSockets: " << IPaddressesSockets << endl;
     for (int i=0; i<NumSocketsMax; ++i){
+    	cout << "IPSocketsList[i]: " << this->IPSocketsList[i] << endl;
     	if (string(this->IPSocketsList[i])==string(IPaddressesSockets)){
     	cout << "Found socket file descriptor//connection to send" << endl;
     	if (string(this->SCmode[i])==string("client")){// Client sends on the file descriptor
