baed94db9e49b0e5d57b3fabb26110eebe33881d
marcjofre <marcjofre@VM-Ubuntu-Tower3420.myguest.virtualbox.org>
2024-01-28 15:56:11 +0100

    Testing synch 8

--- CppScripts/QphysLayerAgent.cpp
+++ CppScripts/QphysLayerAgent.cpp
@@ -117,7 +117,7 @@ int QPLA::countDoubleColons(char* ParamsCharArray) {
 
 int QPLA::countDoubleUnderscores(char* ParamsCharArray) {
   int underscoreCount = 0;
-
+  cout << "ParamsCharArray: " << ParamsCharArray << endl;
   for (int i = 0; ParamsCharArray[i] != '\0'; i++) {
     cout << "ParamsCharArray[i]: " << ParamsCharArray[i] << endl;
     if (ParamsCharArray[i] == '_') {
