09d7298db49d2dad92784911971646cab2bf770b
marcjofre <marcjofre@VM-Ubuntu-Tower3420.myguest.virtualbox.org>
2024-02-06 21:42:37 +0100

    Improving debugging 2

--- CppScripts/BashMakeFileQtransportLayerAgentNCpp.sh
+++ CppScripts/BashMakeFileQtransportLayerAgentNCpp.sh
@@ -1,1 +1,1 @@
-arm-linux-gnueabihf-g++ -static -g -I./BBBhw QtransportLayerAgentN.cpp QnetworkLayerAgent.cpp QlinkLayerAgent.cpp QphysLayerAgent.cpp ./BBBhw/GPIO.cpp -o QtransportLayerAgentN
+arm-linux-gnueabihf-g++ -static -I./BBBhw QtransportLayerAgentN.cpp QnetworkLayerAgent.cpp QlinkLayerAgent.cpp QphysLayerAgent.cpp ./BBBhw/GPIO.cpp -o QtransportLayerAgentN
