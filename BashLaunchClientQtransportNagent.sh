b7f62b6977630a5b469c770815163adf4981dd32
marcjofre <marcjofre@VM-Ubuntu-Tower3420.myguest.virtualbox.org>
2024-02-09 18:20:45 +0100

    Time synch analysis 3

--- BashLaunchClientQtransportNagent.sh
+++ BashLaunchClientQtransportNagent.sh
@@ -1,6 +1,6 @@
 sudo timedatectl set-ntp false
-sudo ./linuxptp/ptp4l -i eth0 -m &
-sudo ./linuxptp/phc2sys -s eth0 -c CLOCK_REALTIME -w -m &
+sudo ./linuxptp/ptp4l -i eth0 &
+sudo ./linuxptp/phc2sys -s eth0 -c CLOCK_REALTIME -w &
 sudo ./CppScripts/QtransportLayerAgentN client 192.168.8.2 192.168.8.1 
 # Kill all the launched processes with same group PID
 kill -- -"$$"
--- BashLaunchServerQtransportNagent.sh
+++ BashLaunchServerQtransportNagent.sh
@@ -1,6 +1,6 @@
 sudo timedatectl set-ntp false
-sudo ./linuxptp/ptp4l -i eth0 -m &
-sudo ./linuxptp/phc2sys -s eth0 -c CLOCK_REALTIME -w -m &
+sudo ./linuxptp/ptp4l -i eth0 &
+sudo ./linuxptp/phc2sys -s eth0 -c CLOCK_REALTIME -w &
 sudo ./CppScripts/QtransportLayerAgentN server 192.168.9.2 192.168.9.1 
 # Kill all the launched processes with same group PID
 kill -- -"$$"
