14c87ad0342aabe81afa9e8eb6bb3de922242747
marcjofre <marcjofre@VM-Ubuntu-Tower3420.myguest.virtualbox.org>
2024-02-09 18:41:55 +0100

    Time synch analysis 5

--- BashLaunchClientQtransportNagent.sh
+++ BashLaunchClientQtransportNagent.sh
@@ -5,4 +5,4 @@ sudo ./linuxptp/phc2sys -s eth0 -c CLOCK_REALTIME -w &
 sudo ./CppScripts/QtransportLayerAgentN client 192.168.8.2 192.168.8.1
 echo 'Stopped PTP as slave'
 # Kill all the launched processes with same group PID
-kill -- -"$$"
+kill -INT $$
--- BashLaunchServerQtransportNagent.sh
+++ BashLaunchServerQtransportNagent.sh
@@ -5,4 +5,4 @@ sudo ./linuxptp/phc2sys -s eth0 -c CLOCK_REALTIME -w &
 sudo ./CppScripts/QtransportLayerAgentN server 192.168.9.2 192.168.9.1
 echo 'Stopped PTP as master'
 # Kill all the launched processes with same group PID
-kill -- -"$$"
+kill -INT $$
