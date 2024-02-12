30519f0951ca9abac8a557b7894fbdef82857f53
marcjofre <marcjofre@Ubuntu-DESKTOP-JMF9ED3-MJ.myguest.virtualbox.org>
2024-02-12 18:26:52 +0100

    PRU starting 25

--- CppScripts/BBBhw/GPIO.cpp
+++ CppScripts/BBBhw/GPIO.cpp
@@ -40,8 +40,8 @@
 #include <stdio.h>
 #include <prussdrv.h>
 #include <pruss_intc_mapping.h>
-#define PRU_Operation_NUM 0 // PRU operation and handling with PRU0
-#define PRU_Signal_NUM 1 // Signals PINS with PRU1
+#define PRU_Operation_NUM 1 // PRU operation and handling with PRU0
+#define PRU_Signal_NUM 0 // Signals PINS with PRU1
 using namespace std;
 
 namespace exploringBB {
