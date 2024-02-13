cb17a69f9f4c830fad5517952ec86e01ff356948
marcjofre <marcjofre@VM-Ubuntu-Tower3420.myguest.virtualbox.org>
2024-02-13 18:09:39 +0100

    PRU development 1

---
+++ BashLaunchClientQtransportNagentNoPTP.sh
@@ -0,0 +1,22 @@
+sudo /etc/init.d/rsyslog stop # stop logging
+echo 'Enabling BBB pins'
+sudo config-pin P9_24 pruin
+sudo config-pin P9_27 pruin
+sudo config-pin P9_28 pruin
+sudo config-pin P9_29 pruin
+sudo config-pin P9_30 pruin
+sudo config-pin P8_27 pruout
+sudo config-pin P8_28 pruout
+sudo config-pin P8_29 pruout
+sudo config-pin P8_30 pruout
+sudo config-pin P8_39 pruout
+sudo config-pin P8_40 pruout
+sudo config-pin P8_41 pruout
+sudo config-pin P8_42 pruout
+sudo config-pin P8_43 pruout
+sudo config-pin P8_44 pruout
+sudo config-pin P8_45 pruout
+sudo config-pin P8_46 pruout
+sudo ./CppScripts/QtransportLayerAgentN client 192.168.8.2 192.168.8.1
+#sudo /etc/init.d/rsyslog start # start logging
+
---
+++ BashLaunchServerQtransportNagentPTP.sh
@@ -0,0 +1,29 @@
+trap "kill 0" EXIT
+echo 'Running PTP as master'
+sudo /etc/init.d/rsyslog stop # stop logging
+sudo timedatectl set-ntp false
+sudo ./linuxptp/ptp4l -i eth0 &
+sudo ./linuxptp/phc2sys -s eth0 -c CLOCK_REALTIME -w &
+echo 'Enabling BBB pins'
+sudo config-pin P9_24 pruin
+sudo config-pin P9_27 pruin
+sudo config-pin P9_28 pruin
+sudo config-pin P9_29 pruin
+sudo config-pin P9_30 pruin
+sudo config-pin P8_27 pruout
+sudo config-pin P8_28 pruout
+sudo config-pin P8_29 pruout
+sudo config-pin P8_30 pruout
+sudo config-pin P8_39 pruout
+sudo config-pin P8_40 pruout
+sudo config-pin P8_41 pruout
+sudo config-pin P8_42 pruout
+sudo config-pin P8_43 pruout
+sudo config-pin P8_44 pruout
+sudo config-pin P8_45 pruout
+sudo config-pin P8_46 pruout
+sudo ./CppScripts/QtransportLayerAgentN server 192.168.9.2 192.168.9.1
+echo 'Stopped PTP as master'
+#sudo /etc/init.d/rsyslog start # start logging
+# Kill all the launched processes with same group PID
+#kill -INT $$
