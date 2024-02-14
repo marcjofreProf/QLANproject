e4c75045dc9831870f10984c01bd6ccbef341492
marcjofre <marcjofre@VM-Ubuntu-Tower3420.myguest.virtualbox.org>
2024-02-14 04:46:37 +0100

    PRU development 10

--- CppScripts/QtransportLayerAgentN.cpp
+++ CppScripts/QtransportLayerAgentN.cpp
@@ -1122,7 +1122,7 @@ int main(int argc, char const * argv[]){
     } // while
   cout << "Exiting the QtransportLayerAgentN" << endl;
   QTLANagent.StopICPconnections(QTLANagent.ParamArgc);
-  QTLANagent.~QTLAN();
+  exit(0);// all Ok
  return 0; // Everything Ok
 }
 
