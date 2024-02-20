f11ef4480adb98cf362f38becc05aaf3f73806e7
marcjofre <marcjofre@Ubuntu-DESKTOP-JMF9ED3-MJ.myguest.virtualbox.org>
2024-02-20 23:46:36 +0100

    Debbuging PRU code 84

--- CppScripts/BBBhw/GPIO.cpp
+++ CppScripts/BBBhw/GPIO.cpp
@@ -287,11 +287,11 @@ for (x=0; x<NumRecords; x++){
 	// First 32 bits is the DWT_CYCCNT of the PRU
 	valCycleCountPRU=*valp;
 	cout << "valCycleCountPRU: " << valCycleCountPRU << endl;
-	valp=valp+4;// 2 times 16 bits
+	valp=valp+2;// 2 times 16 bits
 	// Second 32 bits is the overflow register for DWT_CYCCNT
 	valOverflowCycleCountPRU=*valp;
 	cout << "valOverflowCycleCountPRU: " << valOverflowCycleCountPRU << endl;
-	valp=valp+4;// 2 times 16 bits
+	valp=valp+2;// 2 times 16 bits
 	// Mount the extended counter value
 	extendedCounterPRU=((static_cast<unsigned long long int>(valOverflowCycleCountPRU)) << 31) + (static_cast<unsigned long long int>(valOverflowCycleCountPRU)*auxUnskewingFactor) + static_cast<unsigned long long int>(valCycleCountPRU);// 31 because the overflow counter is increment every half the maxium time for clock (to avoid overflows during execution time)
 	cout << "extendedCounterPRU: " << extendedCounterPRU << endl;
@@ -300,7 +300,7 @@ for (x=0; x<NumRecords; x++){
 	cout << "val: " << std::bitset<16>(val) << endl;
 	valBitsInterest=this->packBits(val); // we're just interested in 4 bits
 	cout << "valBitsInterest: " << std::bitset<16>(valBitsInterest) << endl;
-	valp=valp+2;// 1 times 16 bits
+	valp=valp+1;// 1 times 16 bits
 	//fprintf(outfile, "%d\n", val);
 	streamDDRpru << extendedCounterPRU << valBitsInterest << endl;	
 }
