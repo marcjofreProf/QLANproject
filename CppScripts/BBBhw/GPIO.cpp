9894b4a9fc27dc6a0d2eeb363ec42feb9835fe52
marcjofre <marcjofre@Ubuntu-DESKTOP-JMF9ED3-MJ.myguest.virtualbox.org>
2024-02-20 22:55:20 +0100

    Debbuging PRU code 70

--- CppScripts/BBBhw/GPIO.cpp
+++ CppScripts/BBBhw/GPIO.cpp
@@ -399,12 +399,12 @@ int GPIO::LOCAL_DDMinit(){
     //sharedMem_int   = 	  (unsigned int*)pru_int + SHAREDRAM/4; // Points to start of shared memory
     
     prussdrv_map_prumem(PRUSS0_PRU0_DATARAM, &pru0dataMem);// Maps the PRU0 DRAM memory to input pointer. Memory is then accessed by an array.
-    pru0dataMem_int = (unsigned int*)pru0dataMem+DATARAMoffset;
+    pru0dataMem_int = (unsigned int*)pru0dataMem+DATARAMoffset/4;
     
-    sharedMem_int = (unsigned int*) sharedMem+SHAREDRAM;
+    sharedMem_int = (unsigned int*) sharedMem+SHAREDRAM/4;
     
     prussdrv_map_prumem(PRUSS0_PRU1_DATARAM, &pru1dataMem);// Maps the PRU1 DRAM memory to input pointer. Memory is then accessed by an array.
-    pru1dataMem_int = (unsigned int*)pru1dataMem+DATARAMoffset;   
+    pru1dataMem_int = (unsigned int*)pru1dataMem+DATARAMoffset/4;   
     
 
     return 0;
