f4715955baca7c8bc69d6e15ebf7e8f20ac53151
marcjofre <marcjofre@Ubuntu-DESKTOP-JMF9ED3-MJ.myguest.virtualbox.org>
2024-02-21 13:46:19 +0100

    Optimizing PRUs 14

--- BashLaunchClientQtransportNagentNoPTP.sh
+++ BashLaunchClientQtransportNagentNoPTP.sh
@@ -1,9 +1,9 @@
 sudo /etc/init.d/rsyslog stop # stop logging
 echo 'Enabling BBB pins'
-sudo config-pin P9_27 pruin
 sudo config-pin P9_28 pruin
 sudo config-pin P9_29 pruin
 sudo config-pin P9_30 pruin
+sudo config-pin P9_31 pruin
 sudo config-pin P8_27 pruout
 sudo config-pin P8_28 pruout
 sudo config-pin P8_29 pruout
--- BashLaunchServerQtransportNagentPTP.sh
+++ BashLaunchServerQtransportNagentPTP.sh
@@ -5,10 +5,10 @@ sudo timedatectl set-ntp false
 sudo ./linuxptp/ptp4l -i eth0 &
 sudo ./linuxptp/phc2sys -s eth0 -c CLOCK_REALTIME -w &
 echo 'Enabling BBB pins'
-sudo config-pin P9_27 pruin
 sudo config-pin P9_28 pruin
 sudo config-pin P9_29 pruin
 sudo config-pin P9_30 pruin
+sudo config-pin P9_31 pruin
 sudo config-pin P8_27 pruout
 sudo config-pin P8_28 pruout
 sudo config-pin P8_29 pruout
