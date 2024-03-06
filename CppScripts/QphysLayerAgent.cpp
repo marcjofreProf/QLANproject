c0fcc415c62ae124e3c474773934940bbf4af915
marcjofre <marcjofre@Ubuntu-DESKTOP-JMF9ED3-MJ.myguest.virtualbox.org>
2024-03-06 16:18:45 +0100

    Countdown synch 9

--- CppScripts/QphysLayerAgent.cpp
+++ CppScripts/QphysLayerAgent.cpp
@@ -547,8 +547,10 @@ cout << "Attention TimeTaggsDetAnalytics[6] stores the std wrap count difference
 TimeTaggsDetAnalytics[5]=0.0;
 TimeTaggsDetAnalytics[6]=0.0;
 for (int i=1;i<SimulateNumStoredQubitsNodeAux;i++){
-TimeTaggsDetAnalytics[5]=TimeTaggsDetAnalytics[5]+(1.0/((float)SimulateNumStoredQubitsNodeAux-1.0))*((float)((TimeTaggs[i]-TimeTaggs[i-1])%8));
+//TimeTaggsDetAnalytics[5]=TimeTaggsDetAnalytics[5]+(1.0/((float)SimulateNumStoredQubitsNodeAux-1.0))*((float)((TimeTaggs[i]-TimeTaggs[i-1])%8));
 //TimeTaggsDetAnalytics[5]=TimeTaggsDetAnalytics[5]+(1.0/((float)SimulateNumStoredQubitsNodeAux-1.0))*((float)(fmod(((double)TimeTaggs[i]/(double)TimeTaggs[i-1]),8.0)-1.0));
+// Maximum value to find clock skew
+if (TimeTaggsDetAnalytics[5]<((float)((TimeTaggs[i]-TimeTaggs[i-1])))){TimeTaggsDetAnalytics[5]=((float)((TimeTaggs[i]-TimeTaggs[i-1])));}
 }
 
 for (int i=1;i<SimulateNumStoredQubitsNodeAux;i++){
