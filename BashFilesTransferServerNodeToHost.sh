d816cd188d386938990fb9d2c228e5f4be00aabf
marcjofre <marcjofre@Ubuntu-DESKTOP-JMF9ED3-MJ.myguest.virtualbox.org>
2024-03-15 10:14:02 +0100

    More detailed results 3

---
+++ BashFilesTransferServerNodeToHost.sh
@@ -0,0 +1,4 @@
+# It needs installed in the host computer: sudo apt install openssh-server
+sudo scp /home/debian/Scripts/QLANproject/PRUdata/TimetaggingData marcjofre@10.0.0.1:/home/marcjofre/Scripts/QLANproject/PRUdata/ServerRawStoredQubitsNode
+echo 'Sent files from server node to host'
+
