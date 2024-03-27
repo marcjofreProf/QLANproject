c1050013b0360d8660211d618fc91e01b318dc78
marcjofre <marcjofre@VM-Ubuntu-Tower3420.myguest.virtualbox.org>
2024-03-27 19:12:40 +0100

    Kernel Phys clock 12

--- BBBclockKernelPhysical/BBBclockKernelPhysicalDaemon.cpp
+++ BBBclockKernelPhysical/BBBclockKernelPhysicalDaemon.cpp
@@ -7,7 +7,7 @@
 #include <iostream>
 #include <unistd.h> //for sleep
 #include <signal.h>
-#define WaitTimeAfterMainWhileLoop 10000 //nanoseconds
+#define WaitTimeAfterMainWhileLoop 10000000 //nanoseconds
 // Time/synchronization management
 #include <chrono>
 // PRU programming
