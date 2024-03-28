ebe3d830b0d6b3becb476cdc9a1268982e24b6e9
marcjofre <marcjofre@VM-Ubuntu-Tower3420.myguest.virtualbox.org>
2024-03-28 15:18:28 +0100

    Clockcs 6

--- BashFilesTransferDealerNodeToHost.sh
+++ BashFilesTransferDealerNodeToHost.sh
@@ -1,6 +1,6 @@
 # It needs installed in the host computer: sudo apt install openssh-server
 # ./BashFilesTransferDealerNodeToHost.sh arg1
 # arg1: to specify if NTP or PTP or others
-sudo scp /home/debian/Scripts/QLANproject/PRUdata/TimetaggingData marcjofre@10.0.0.1:/home/marcjofre/Scripts/QLANproject/PRUdata/DealerRawStoredQubitsNode$1
+sudo scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null /home/debian/Scripts/QLANproject/PRUdata/TimetaggingData marcjofre@10.0.0.1:/home/marcjofre/Scripts/QLANproject/PRUdata/DealerRawStoredQubitsNode$1
 echo 'Sent files from dealer node to host'
 
