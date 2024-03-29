0f75493d1abe81145c82a34b64b2359065356c10
marcjofre <marcjofre@VM-Ubuntu-Tower3420.myguest.virtualbox.org>
2024-03-29 14:28:16 +0100

    PID clocks 2

--- BBBclockKernelPhysical/BBBclockKernelPhysicalDaemon.h
+++ BBBclockKernelPhysical/BBBclockKernelPhysicalDaemon.h
@@ -49,8 +49,8 @@ private:// Variables
 	TimePoint TimePointClockCurrentInitial=std::chrono::time_point<Clock>(); // Initial updated value of the clock (updated in each iteration)
 	// PRU clock handling
 	unsigned int NumClocksHalfPeriodPRUclock=(unsigned int)(0.5*((double)(ClockPeriodNanoseconds))/((double)(PRUclockStepPeriodNanoseconds)));// set the number of clocks that defines the half period of the clock. For 32Khz, with a PRU clock of 5ns is 6250
-	float RatioFreqAdjustment=0.8;// Maximum and minimum frequency variation allowed
-	float RatioAverageFactorClockHalfPeriod=0.9999999; // The lower the more aggresive taking the new computed values
+	float RatioFreqAdjustment=0.4;// Maximum and minimum frequency variation allowed
+	float RatioAverageFactorClockHalfPeriod=0.1; // The lower the more aggresive taking the new computed values
 	unsigned int MinNumClocksHalfPeriodPRUclock=(unsigned int)((1.0-RatioFreqAdjustment)*(float)(NumClocksHalfPeriodPRUclock));
 	unsigned int MaxNumClocksHalfPeriodPRUclock=(unsigned int)((1.0+RatioFreqAdjustment)*(float)(NumClocksHalfPeriodPRUclock));
 	int retInterruptsPRU0;
