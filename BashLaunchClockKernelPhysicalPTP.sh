c46b6030d08df956753bd9f30c22e28be126aee8
marcjofre <marcjofre@VM-Ubuntu-Tower3420.myguest.virtualbox.org>
2024-03-31 11:55:52 +0200

    Better scripts synch 4

--- BashLaunchClockKernelPhysicalPTP.sh
+++ BashLaunchClockKernelPhysicalPTP.sh
@@ -6,10 +6,10 @@
 trap "kill 0" EXIT
 echo 'Running PTP'
 sudo /etc/init.d/rsyslog stop # stop logging
-sudo systemctl stop systemd-timesyncd # start system synch
+sudo systemctl start systemd-timesyncd # start system synch
 sudo timedatectl set-ntp false
-#sudo ./linuxptp/ptp4l -i eth0 -f PTP4lConfigQLANproject.cfg & #-m
-#sudo ./linuxptp/phc2sys -s eth0 -c CLOCK_REALTIME -w & #-f PTP2pcConfigQLANprojectSlave.cfg & -m
+sudo ./linuxptp/ptp4l -i eth0 -f PTP4lConfigQLANproject.cfg & #-m
+sudo ./linuxptp/phc2sys -s eth0 -c CLOCK_REALTIME -w & #-f PTP2pcConfigQLANprojectSlave.cfg & -m
 echo 'Enabling BBB pins'
 sudo config-pin P9_28 pruin
 sudo config-pin P9_29 pruin
