de8d3c04ed6ef3dbe165d299e16da7e61975a710
marcjofre <marcjofre@VM-Ubuntu-Tower3420.myguest.virtualbox.org>
2024-04-02 17:28:28 +0200

    Timer enable 7

--- BashLaunchClockKernelPhysicalPTP.sh
+++ BashLaunchClockKernelPhysicalPTP.sh
@@ -11,8 +11,8 @@ sudo ./linuxptp/ptp4l -i eth0 -f PTP4lConfigQLANproject.cfg & #-m
 sudo ./linuxptp/phc2sys -s eth0 -c CLOCK_REALTIME -w & #-f PTP2pcConfigQLANprojectSlave.cfg & -m
 echo 'Enabling PWM for 24 MHz ref clock'
 sudo config-pin P8.19 pwm
-sudo sudo sh -c "echo '38' >> /sys/class/pwm/pwmchip7/pwm-7\:0/period"
-sudo sudo sh -c "echo '19' >> /sys/class/pwm/pwmchip7/pwm-7\:0/duty_cycle"
+sudo sudo sh -c "echo '36' >> /sys/class/pwm/pwmchip7/pwm-7\:0/period"
+sudo sudo sh -c "echo '18' >> /sys/class/pwm/pwmchip7/pwm-7\:0/duty_cycle"
 sudo sudo sh -c "echo '1' >> /sys/class/pwm/pwmchip7/pwm-7\:0/enable"
 echo 'Enabling BBB pins'
 sudo config-pin P9_28 pruin
