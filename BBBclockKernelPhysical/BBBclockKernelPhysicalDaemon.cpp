71e6185d3b86778544fca7bde4961cd1464ec416
marcjofre <marcjofre@VM-Ubuntu-Tower3420.myguest.virtualbox.org>
2024-04-05 20:11:57 +0200

    Clock generation 2

--- BBBclockKernelPhysical/BBBclockKernelPhysicalDaemon.cpp
+++ BBBclockKernelPhysical/BBBclockKernelPhysicalDaemon.cpp
@@ -8,7 +8,7 @@
 #include <unistd.h> //for sleep
 #include <signal.h>
 #include <cstring>
-#define WaitTimeAfterMainWhileLoop 900000000 //nanoseconds
+#define WaitTimeAfterMainWhileLoop 990000000 //nanoseconds. Maximum 999999999
 // Time/synchronization management
 #include <chrono>
 // PRU programming
