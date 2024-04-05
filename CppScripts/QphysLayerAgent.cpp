3f40942ca204e77dbe7b47656e0aefbd82884ae6
marcjofre <marcjofre@Ubuntu-DESKTOP-JMF9ED3-MJ.myguest.virtualbox.org>
2024-04-05 12:01:57 +0200

    Inter synch 40

--- CppScripts/QphysLayerAgent.cpp
+++ CppScripts/QphysLayerAgent.cpp
@@ -562,11 +562,11 @@ TimeTaggsDetAnalytics[7]=(double)(TimeTaggs[0]);
 TimeTaggsDetAnalytics[5]=0.0;
 TimeTaggsDetAnalytics[6]=0.0;
 for (int i=0;i<(SimulateNumStoredQubitsNodeAux-1);i++){
-TimeTaggsDetAnalytics[5]=TimeTaggsDetAnalytics[5]+(1.0/((double)SimulateNumStoredQubitsNodeAux-1.0))*((double)((HistPeriodicityAux/2+TimeTaggs[i+1]-TimeTaggs[i])%HistPeriodicityAux)-(double)(HistPeriodicityAux/2)+HistPeriodicityAux);
+TimeTaggsDetAnalytics[5]=TimeTaggsDetAnalytics[5]+(1.0/((double)SimulateNumStoredQubitsNodeAux-1.0))*((double)((HistPeriodicityAux/2+TimeTaggs[i+1]-TimeTaggs[i])%HistPeriodicityAux)-(double)(HistPeriodicityAux/2)+(double)(HistPeriodicityAux/8));
 }
 
 for (int i=0;i<(SimulateNumStoredQubitsNodeAux-1);i++){
-TimeTaggsDetAnalytics[6]=TimeTaggsDetAnalytics[6]+(1.0/((double)SimulateNumStoredQubitsNodeAux-1.0))*pow(((double)((HistPeriodicityAux/2+TimeTaggs[i+1]-TimeTaggs[i])%HistPeriodicityAux)-(double)(HistPeriodicityAux/2)+HistPeriodicityAux)-TimeTaggsDetAnalytics[5],2.0);
+TimeTaggsDetAnalytics[6]=TimeTaggsDetAnalytics[6]+(1.0/((double)SimulateNumStoredQubitsNodeAux-1.0))*pow(((double)((HistPeriodicityAux/2+TimeTaggs[i+1]-TimeTaggs[i])%HistPeriodicityAux)-(double)(HistPeriodicityAux/2)+(double)(HistPeriodicityAux/8))-TimeTaggsDetAnalytics[5],2.0);
 }
 TimeTaggsDetAnalytics[6]=sqrt(TimeTaggsDetAnalytics[6]);
 
