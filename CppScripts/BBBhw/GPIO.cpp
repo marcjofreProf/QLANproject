bd5061d54c56d10f5a361d38f80d96355762f4cf
marcjofre <marcjofre@Ubuntu-DESKTOP-JMF9ED3-MJ.myguest.virtualbox.org>
2024-04-05 10:21:19 +0200

    Inter synch 28

--- CppScripts/BBBhw/GPIO.cpp
+++ CppScripts/BBBhw/GPIO.cpp
@@ -569,7 +569,7 @@ int GPIO::RetrieveNumStoredQuBits(unsigned long long int* TimeTaggs, unsigned ch
 		    // Apply pulses time drift correction
 		    if (NumSynchPulsesRed>1){// There is a calibration curve
 		        // using doubles, to represent usigned long long int can hold, with the 5ns PRU count, up to 2 years with presition!!!
-		    	TimeTaggs[lineCount]=(unsigned long long int)(((double)(ValueReadTest))*AdjPulseSynchCoeff);
+		    	TimeTaggs[lineCount]=(unsigned long long int)(((double)(ValueReadTest))/AdjPulseSynchCoeff);
 		    }
 		    else{// No pulse compensation
 		    	TimeTaggs[lineCount]=ValueReadTest; 
@@ -581,6 +581,7 @@ int GPIO::RetrieveNumStoredQuBits(unsigned long long int* TimeTaggs, unsigned ch
 	    	    lineCount++; // Increment line count for each line read
 	    	    streamDDRpru.clear(); // will reset these state flags, allowing you to continue using the stream for additional I/O operations 	    
 	    	    }
+	    	if (lineCount==0){cout << "RetrieveNumStoredQuBits: No timetaggs present!" << endl;}
 		return lineCount;
 	}
 	else{
