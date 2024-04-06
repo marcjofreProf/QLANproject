85c73b8665d0acb211720d75891d4a31a7c6b3a4
marcjofre <marcjofre@Ubuntu-DESKTOP-JMF9ED3-MJ.myguest.virtualbox.org>
2024-04-06 15:52:14 +0200

    Inter synch 79

--- CppScripts/BBBhw/GPIO.cpp
+++ CppScripts/BBBhw/GPIO.cpp
@@ -549,7 +549,7 @@ int GPIO::RetrieveNumStoredQuBits(unsigned long long int* TimeTaggs, unsigned ch
 				CoeffSynchAdjAux1=(double)(CoeffSynchAdjAux0*PeriodCountsPulseAdj);//((double)((SynchPulsesTags[iIter+1]-SynchPulsesTags[iIter])/((unsigned long long int)(PeriodCountsPulseAdj))));
 				CoeffSynchAdjAux2=(double)(SynchPulsesTags[iIter+1]-SynchPulsesTags[iIter]);//(((double)(SynchPulsesTags[iIter+1]-SynchPulsesTags[iIter]))/(PeriodCountsPulseAdj));
 				if (CoeffSynchAdjAux1!=0.0 and CoeffSynchAdjAux2!=0.0){
-					AdjPulseSynchCoeff=AdjPulseSynchCoeff+(CoeffSynchAdjAux1/CoeffSynchAdjAux2)/((double)(CoeffSynchAdjAux0));
+					AdjPulseSynchCoeff=AdjPulseSynchCoeff+(CoeffSynchAdjAux1/CoeffSynchAdjAux2);
 					NumAvgAux++;
 				}
 			}
