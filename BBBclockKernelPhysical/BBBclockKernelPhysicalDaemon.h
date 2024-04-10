9126bc7ef9a52c8743507dace07db3debb775cdf
marcjofre <marcjofre@Ubuntu-DESKTOP-JMF9ED3-MJ.myguest.virtualbox.org>
2024-04-10 12:40:59 +0200

    Clock drift 19

--- BBBclockKernelPhysical/BBBclockKernelPhysicalDaemon.h
+++ BBBclockKernelPhysical/BBBclockKernelPhysicalDaemon.h
@@ -83,7 +83,7 @@ private:// Variables
 	double NumClocksHalfPeriodPRUclockArray[MaxMedianFilterArraySize]={NumClocksHalfPeriodPRUclock};
 	int TimePointClockCurrentAdjFilErrorArray[MaxMedianFilterArraySize]={0};
 	// PID error correction
-	double PIDconstant=1.1;
+	double PIDconstant=1.1; // should be larger than 1.0
 
 public:	// Functions/Methods
 	CKPD(); //constructor	
--- CppScripts/BBBhw/GPIO.cpp
+++ CppScripts/BBBhw/GPIO.cpp
@@ -568,11 +568,11 @@ int GPIO::RetrieveNumStoredQuBits(unsigned long long int* TimeTaggs, unsigned ch
 			for (int iIter=0;iIter<(NumSynchPulsesRed-3);iIter++){
 				CoeffSynchAdjAux0=(unsigned long long int)(((double)(SynchPulsesTags[iIter+2]-SynchPulsesTags[iIter+1])+PeriodCountsPulseAdj/2.0)/PeriodCountsPulseAdj); // Distill how many pulse synch periods passes...1, 2, 3....To round ot the nearest integer value add half of the dividend to the divisor
 				CoeffSynchAdjAux1=(double)(CoeffSynchAdjAux0)*PeriodCountsPulseAdj;//((double)((SynchPulsesTags[iIter+1]-SynchPulsesTags[iIter])/((unsigned long long int)(PeriodCountsPulseAdj))));
-				CoeffSynchAdjAux3=(double)((unsigned long long int)(((double)(SynchPulsesTags[iIter+1]-SynchPulsesTags[iIter+0])+PeriodCountsPulseAdj/2.0)/PeriodCountsPulseAdj))*1.0; // Distill how many pulse synch periods passes...1, 2, 3....To round ot the nearest integer value add half of the dividend to the divisor
-				CoeffSynchAdjAux4=(double)((unsigned long long int)(((double)(SynchPulsesTags[iIter+3]-SynchPulsesTags[iIter+2])+PeriodCountsPulseAdj/2.0)/PeriodCountsPulseAdj))*1.0; // Distill how many pulse synch periods passes...1, 2, 3....To round ot the nearest integer value add half of the dividend to the divisor
+				CoeffSynchAdjAux3=(double)((unsigned long long int)(((double)(SynchPulsesTags[iIter+1]-SynchPulsesTags[iIter+0])+PeriodCountsPulseAdj/2.0)/PeriodCountsPulseAdj))*PeriodCountsPulseAdj; // Distill how many pulse synch periods passes...1, 2, 3....To round ot the nearest integer value add half of the dividend to the divisor
+				CoeffSynchAdjAux4=(double)((unsigned long long int)(((double)(SynchPulsesTags[iIter+3]-SynchPulsesTags[iIter+2])+PeriodCountsPulseAdj/2.0)/PeriodCountsPulseAdj))*PeriodCountsPulseAdj; // Distill how many pulse synch periods passes...1, 2, 3....To round ot the nearest integer value add half of the dividend to the divisor
 				if (CoeffSynchAdjAux3!=0.0 and CoeffSynchAdjAux4!=0.0){CoeffSynchAdjAux2=(double)(SynchPulsesTags[iIter+3]-SynchPulsesTags[iIter+2])/CoeffSynchAdjAux4-(double)(SynchPulsesTags[iIter+1]-SynchPulsesTags[iIter+0])/CoeffSynchAdjAux3;}//(((double)(SynchPulsesTags[iIter+1]-SynchPulsesTags[iIter]))/(PeriodCountsPulseAdj));
 				if (CoeffSynchAdjAux1!=0.0 and CoeffSynchAdjAux3!=0.0 and CoeffSynchAdjAux4!=0.0){
-					AdjPulseSynchCoeff=AdjPulseSynchCoeff+(CoeffSynchAdjAux2/(CoeffSynchAdjAux1+CoeffSynchAdjAux3/2.0+CoeffSynchAdjAux4/2.0));//+(CoeffSynchAdjAux2/CoeffSynchAdjAux1);
+					AdjPulseSynchCoeff=AdjPulseSynchCoeff+CoeffSynchAdjAux2;//+(CoeffSynchAdjAux2/CoeffSynchAdjAux1);
 					NumAvgAux++;
 				}
 			}
