407e41ca0f42fc3d0e2884dde1270e965ef1e1bd
marcjofre <marcjofre@Ubuntu-DESKTOP-JMF9ED3-MJ.myguest.virtualbox.org>
2024-04-10 21:03:13 +0200

    Clock drift 60

--- BBBclockKernelPhysical/BBBclockKernelPhysicalDaemon.cpp
+++ BBBclockKernelPhysical/BBBclockKernelPhysicalDaemon.cpp
@@ -218,7 +218,7 @@ break;
 }
 case 1:{// Median implementation
 this->TimePointClockCurrentAdjFilErrorArray[this->CounterHandleInterruptSynchPRU%MedianFilterFactor]=this->TimePointClockCurrentAdjError;// Error to be compensated for
-this->TimePointClockCurrentAdjFilError=this->IMedianFilterSubArray(this->TimePointClockCurrentAdjFilErrorArray);
+this->TimePointClockCurrentAdjFilError=this->TimePointClockCurrentAdjError;//this->IMedianFilterSubArray(this->TimePointClockCurrentAdjFilErrorArray);
 break;
 }
 default:{// Average implementation
@@ -235,7 +235,7 @@ break;
 }
 case 1:{// Median implementation
 this->TimePointClockCurrentFinalInitialAdj_time_as_countArray[this->CounterHandleInterruptSynchPRU%MedianFilterFactor]=std::chrono::duration_cast<std::chrono::nanoseconds>(duration_FinalInitialAdj).count();
-this->TimePointClockCurrentFinalInitialAdj_time_as_count=this->ULLIMedianFilterSubArray(this->TimePointClockCurrentFinalInitialAdj_time_as_countArray);
+this->TimePointClockCurrentFinalInitialAdj_time_as_count=std::chrono::duration_cast<std::chrono::nanoseconds>(duration_FinalInitialAdj).count();//this->ULLIMedianFilterSubArray(this->TimePointClockCurrentFinalInitialAdj_time_as_countArray);
 break;
 }
 default:{// Average implementation
@@ -262,7 +262,7 @@ break;
 }
 case 1:{// Median implementation
 this->NumClocksHalfPeriodPRUclockArray[this->CounterHandleInterruptSynchPRU%MedianFilterFactor]=this->NumClocksHalfPeriodPRUclockUpdated;
-this->NumClocksHalfPeriodPRUclock=this->DoubleMedianFilterSubArray(this->NumClocksHalfPeriodPRUclockArray);
+this->NumClocksHalfPeriodPRUclock=this->NumClocksHalfPeriodPRUclockUpdated;//this->DoubleMedianFilterSubArray(this->NumClocksHalfPeriodPRUclockArray);
 break;
 }
 default:{
