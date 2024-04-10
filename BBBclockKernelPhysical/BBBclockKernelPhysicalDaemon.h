78d15f2544739ea300a805ae9021dafb0187022e
marcjofre <marcjofre@Ubuntu-DESKTOP-JMF9ED3-MJ.myguest.virtualbox.org>
2024-04-10 20:21:19 +0200

    Clock drift 54

--- BBBclockKernelPhysical/BBBclockKernelPhysicalDaemon.h
+++ BBBclockKernelPhysical/BBBclockKernelPhysicalDaemon.h
@@ -20,9 +20,9 @@
 #define WaitTimeAfterMainWhileLoop 990000000 //nanoseconds. Maximum 999999999. Adjusted to have 10ms time slot to activate the busy wait
 #define PRUclockStepPeriodNanoseconds		5 // PRU clock cycle time in nanoseconds. Specs says 5ns, but maybe more realistic is 
 #define ClockCyclePeriodAdjustment		1// pps// 65536 32 KHz // Very important parameter. The larger the better, since the interrupts time jitter do not paly a role, as long as the PRU counter does not overexceed (the turn down is that ht eupdate time is larger)
-#define WaitCyclesBeforeAveraging	25 // To go into steady state in the initialization
+#define WaitCyclesBeforeAveraging	20 // To go into steady state in the initialization
 #define MaxMedianFilterArraySize	50
-#define FilterMode 1 // 0: averaging; 1: median; 2: mean window. The erro jumps between two values, then maybe it is better to use mea window
+#define FilterMode 2 // 0: averaging; 1: median; 2: mean window. The erro jumps between two values, then maybe it is better to use mea window
 
 namespace exploringBBBCKPD {
 
