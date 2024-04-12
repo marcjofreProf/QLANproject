d12907cbfd1d64f7bcd94b889689deee1f850a9f
marcjofre <marcjofre@Ubuntu-DESKTOP-JMF9ED3-MJ.myguest.virtualbox.org>
2024-04-13 00:17:51 +0200

    Both system_clock and steady_clock 23

--- BBBclockKernelPhysical/BBBclockKernelPhysicalDaemon.cpp
+++ BBBclockKernelPhysical/BBBclockKernelPhysicalDaemon.cpp
@@ -313,7 +313,7 @@ return 0;// all ok
 struct timespec CKPD::SetWhileWait(){
 	struct timespec requestWhileWaitAux;
 	this->TimePointClockCurrentFinal=this->TimePointClockCurrentInitial-std::chrono::nanoseconds(this->TimeAdjPeriod)+std::chrono::nanoseconds(static_cast<unsigned long long int>(this->PIDconstant*static_cast<double>(this->TimePointClockCurrentAdjFilError)));
-	this->TimePointClockCurrentInitial=this->TimePointClockCurrentFinal+std::chrono::nanoseconds(static_cast<unsigned long long int>(this->PIDconstant*static_cast<double>(this->TimePointClockCurrentAdjFilError))); //Update value
+	this->TimePointClockCurrentInitial=this->TimePointClockCurrentFinal;//-std::chrono::nanoseconds(static_cast<unsigned long long int>(this->PIDconstant*static_cast<double>(this->TimePointClockCurrentAdjFilError))); //Update value
 	auto duration_since_epochFutureTimePoint=this->TimePointClockCurrentFinal.time_since_epoch();
 	// Convert duration to desired time
 	unsigned long long int TimePointClockCurrentFinal_time_as_count = std::chrono::duration_cast<std::chrono::nanoseconds>(duration_since_epochFutureTimePoint).count(); // Convert 
@@ -326,7 +326,7 @@ struct timespec CKPD::SetWhileWait(){
 
 int CKPD::SetFutureTimePoint(){
 	this->TimePointClockCurrentFinal=this->TimePointClockCurrentInitial-std::chrono::nanoseconds(this->TimeAdjPeriod)+std::chrono::nanoseconds(static_cast<unsigned long long int>(this->PIDconstant*static_cast<double>(this->TimePointClockCurrentAdjFilError)));
-	this->TimePointClockCurrentInitial=this->TimePointClockCurrentFinal+std::chrono::nanoseconds(static_cast<unsigned long long int>(this->PIDconstant*static_cast<double>(this->TimePointClockCurrentAdjFilError))); //Update value
+	this->TimePointClockCurrentInitial=this->TimePointClockCurrentFinal;//-std::chrono::nanoseconds(static_cast<unsigned long long int>(this->PIDconstant*static_cast<double>(this->TimePointClockCurrentAdjFilError))); //Update value
 	return 0; // All Ok
 }
 
