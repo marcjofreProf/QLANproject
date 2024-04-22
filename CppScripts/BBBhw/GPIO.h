7680a15ba1970af5ea8da1945bdd74e48c3376c1
marcjofre <marcjofre@Ubuntu-DESKTOP-JMF9ED3-MJ.myguest.virtualbox.org>
2024-04-22 20:49:29 +0200

    Auto snch 4

--- CppScripts/BBBhw/GPIO.h
+++ CppScripts/BBBhw/GPIO.h
@@ -77,7 +77,7 @@ private:// Variables
 	TimePoint TimePointClockCurrentPRU0meas=std::chrono::time_point<Clock>();
 	TimePoint TimePointClockCurrentPRU0measOld=std::chrono::time_point<Clock>();
 	unsigned long long int TimePRU1synchPeriod=500000000;//2684354560;//21474836480; // almost 1 second in nanoseconds, since captures due to tthe clock resolution for timetaggs cannot last than 2 or 2.5 seconds
-	unsigned long long int TimePRU1synchPeriodMargin=10000;// Margin to enter and correct for PRU timer reset, in nanoseconds
+	unsigned long long int TimePRU1synchPeriodMargin=100000;// Margin to enter and correct for PRU timer reset, in nanoseconds
 	struct timespec requestWhileWait;
 	TimePoint TimePointClockCurrentSynchPRU1future=std::chrono::time_point<Clock>();// For synch purposes
 	unsigned long long int TimeElpasedNow_time_as_count=0;
