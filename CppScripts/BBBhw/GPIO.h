4a1cdffff2dc52a727ab94181416128dcff01b1a
marcjofre <marcjofre@Ubuntu-DESKTOP-JMF9ED3-MJ.myguest.virtualbox.org>
2024-04-23 21:29:27 +0200

    PID synch 19

--- CppScripts/BBBhw/GPIO.h
+++ CppScripts/BBBhw/GPIO.h
@@ -79,9 +79,9 @@ private:// Variables
 	unsigned long long int iIterPRUcurrentTimerValLast=0;
 	double EstimateSynch=1.0;
 	// PID error correction
-	double SynchAdjconstant=0.001;
-	double PIDconstant=0.075;
-	double PIDintegral=0.0020;
+	double SynchAdjconstant=0.01;
+	double PIDconstant=0.0075;
+	double PIDintegral=0.005;
 	double PIDderiv=0.005;	
 	// Time/synchronization management
 	using Clock = std::chrono::system_clock;//steady_clock;// We do not use system clock because we do not need a watch, but instead we use steady_clock because we need a chrono /system_clock;steady_clock;high_resolution_clock
