964a9f864993d4a6f8d42d7929464a7836af33b8
marcjofre <marcjofre@Ubuntu-DESKTOP-JMF9ED3-MJ.myguest.virtualbox.org>
2024-04-24 18:05:38 +0200

    PID synch 65

--- CppScripts/BBBhw/GPIO.cpp
+++ CppScripts/BBBhw/GPIO.cpp
@@ -320,7 +320,7 @@ int GPIO::PRUsignalTimerSynch(){
 					}
 					
 					// Computations for Synch calculaton for PRU0 compensation
-					this->EstimateSynch=static_cast<double>(((this->PRUcurrentTimerVal-0*this->PRUoffsetDriftErrorAppliedRaw)-(this->PRUcurrentTimerValOld-0*this->PRUoffsetDriftErrorAppliedOldRaw)))/(static_cast<double>(this->iIterPRUcurrentTimerValPass*this->TimePRU1synchPeriod)/static_cast<double>(PRUclockStepPeriodNanoseconds));
+					this->EstimateSynch=static_cast<double>(((this->PRUcurrentTimerVal-this->PRUoffsetDriftErrorAppliedRaw)-(this->PRUcurrentTimerValOld-this->PRUoffsetDriftErrorAppliedOldRaw)))/(static_cast<double>(this->iIterPRUcurrentTimerValPass*this->TimePRU1synchPeriod)/static_cast<double>(PRUclockStepPeriodNanoseconds));
 					this->EstimateSynch=1.0+this->SynchAdjconstant*(this->EstimateSynch-1.0);
 					//this->EstimateSynch=1.0; // To disable synch adjustment
 					
