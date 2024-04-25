8d1c6ce584604576b4b9f5fe7b55031b313202f6
marcjofre <marcjofre@Ubuntu-DESKTOP-JMF9ED3-MJ.myguest.virtualbox.org>
2024-04-25 08:27:22 +0200

    PID synch 96

--- CppScripts/BBBhw/GPIO.cpp
+++ CppScripts/BBBhw/GPIO.cpp
@@ -251,7 +251,7 @@ int GPIO::PRUsignalTimerSynch(){
 	while(true){		
 		//if (Clock::now()<=(this->TimePointClockCurrentSynchPRU1future-std::chrono::nanoseconds(this->TimeClockMarging))){// It was possible to execute when needed
 			//cout << "Resetting PRUs timer!" << endl;
-			if (clock_nanosleep(CLOCK_REALTIME,TIMER_ABSTIME,&requestWhileWait,NULL)==0 and this->ManualSemaphore==false){// Synch barrier. CLOCK_TAI (with steady_clock) instead of CLOCK_REALTIME (with system_clock)
+			if (clock_nanosleep(CLOCK_TAI,TIMER_ABSTIME,&requestWhileWait,NULL)==0 and this->ManualSemaphore==false){// Synch barrier. CLOCK_TAI (with steady_clock) instead of CLOCK_REALTIME (with system_clock)
 				this->acquire();
 				this->ManualSemaphore=true;
 				// Important, the following line at the very beggining to reduce the command jitter
