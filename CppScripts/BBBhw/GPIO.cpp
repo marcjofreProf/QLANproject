47043cf600d907955a2c646089e4d3ab2c03e2d6
marcjofre <marcjofre@Ubuntu-DESKTOP-JMF9ED3-MJ.myguest.virtualbox.org>
2024-04-26 22:15:55 +0200

    Adjustments 5

--- CppScripts/BBBhw/GPIO.cpp
+++ CppScripts/BBBhw/GPIO.cpp
@@ -1,30 +1,3 @@
-/*
- * GPIO.cpp  Created on: 29 Apr 2014
- * Copyright (c) 2014 Derek Molloy (www.derekmolloy.ie)
- * Made available for the book "Exploring BeagleBone"
- * If you use this code in your work please cite:
- *   Derek Molloy, "Exploring BeagleBone: Tools and Techniques for Building
- *   with Embedded Linux", Wiley, 2014, ISBN:9781118935125.
- * See: www.exploringbeaglebone.com
- * Licensed under the EUPL V.1.1
- *
- * This Software is provided to You under the terms of the European
- * Union Public License (the "EUPL") version 1.1 as published by the
- * European Union. Any use of this Software, other than as authorized
- * under this License is strictly prohibited (to the extent such use
- * is covered by a right of the copyright holder of this Software).
- *
- * This Software is provided under the License on an "AS IS" basis and
- * without warranties of any kind concerning the Software, including
- * without limitation merchantability, fitness for a particular purpose,
- * absence of defects or errors, accuracy, and non-infringement of
- * intellectual property rights other than copyright. This disclaimer
- * of warranty is an essential part of the License and a condition for
- * the grant of any rights to this Software.
- *
- * For more details, see http://www.derekmolloy.ie/
- */
-
 #include "GPIO.h"
 #include<iostream>
 #include<fstream>
