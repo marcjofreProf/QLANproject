978e02ed54231d9a604b89a8c9d9bf80566c8ee5
marcjofre <marcjofre@Ubuntu-DESKTOP-JMF9ED3-MJ.myguest.virtualbox.org>
2024-04-26 11:16:22 +0200

    PID synch 160

--- CppScripts/BBBhw/GPIO.cpp
+++ CppScripts/BBBhw/GPIO.cpp
@@ -304,8 +304,8 @@ int GPIO::PRUsignalTimerSynch(){
 					this->EstimateSynchDirection=(static_cast<double>(this->PRUcurrentTimerVal-1*this->PRUoffsetDriftErrorAppliedRaw))-(static_cast<double>(this->PRUcurrentTimerValOld-0*this->PRUoffsetDriftErrorAppliedOldRaw)+(static_cast<double>(this->iIterPRUcurrentTimerValPass*this->TimePRU1synchPeriod)/static_cast<double>(PRUclockStepPeriodNanoseconds)));
 					EstimateSynchDirectionArray[iIterPRUcurrentTimerValSynch%NumSynchMeasAvgAux]=this->EstimateSynchDirection;
 					this->EstimateSynchDirectionAvg=DoubleMedianFilterSubArray(EstimateSynchDirectionArray,NumSynchMeasAvgAux);
-					if (this->EstimateSynchDirectionAvg<=0.0){this->PRUoffsetDriftErrorAppliedCorrectionDirection=1;}
-					else{this->PRUoffsetDriftErrorAppliedCorrectionDirection=1;}
+					if (this->EstimateSynchDirectionAvg<=0.0){this->PRUoffsetDriftErrorAppliedCorrectionDirection=-1;}
+					else{this->PRUoffsetDriftErrorAppliedCorrectionDirection=-1;}
 					//this->EstimateSynch=1.0; // To disable synch adjustment
 									
 					//// PID error computation to correct for signal PRU 1 generation								
