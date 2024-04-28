76fe5ea8b88af97f218d76b54511bb0a8444d517
marcjofre <marcjofre@Ubuntu-DESKTOP-JMF9ED3-MJ.myguest.virtualbox.org>
2024-04-28 18:17:17 +0200

    Adjustments 95

--- CppScripts/BBBhw/GPIO.h
+++ CppScripts/BBBhw/GPIO.h
@@ -74,7 +74,7 @@ private:// Variables
 	double EstimateSynchDirectionArray[MaxNumPulses]={0.0};
 	// PID error correction
 	double SynchAdjconstant=1.0;// 
-	double PIDconstantAdvancing=0.75;// Too close to 1.0 makes it unstable and too much correction
+	double PIDconstantAdvancing=0.80;// Too close to 1.0 makes it unstable and too much correction
 	double PIDconstantDelaying=PIDconstantAdvancing;// Too close to 1.0 makes it unstable and too much correction
 	double PIDintegral=0.0;
 	double PIDderiv=0.0;	
