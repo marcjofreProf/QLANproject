1297f6e1eadd0218b52b8a9ce51489c750eb0679
marcjofre <marcjofre@Ubuntu-DESKTOP-JMF9ED3-MJ.myguest.virtualbox.org>
2024-04-29 14:16:43 +0200

    Adjustments 129

--- BashLaunchClientQtransportNagentPTP.sh
+++ BashLaunchClientQtransportNagentPTP.sh
@@ -2,6 +2,7 @@ trap "kill 0" EXIT
 echo 'Running PTP'
 sudo /etc/init.d/rsyslog stop # stop logging
 sudo systemctl stop systemd-timesyncd # stop system synch
+sudo systemctl disable systemd-timesyncd # disable system synch
 sudo timedatectl set-ntp false
 sudo ./linuxptp/ptp4l -i eth0 -s & # -f PTP4lConfigQLANprojectSlave.cfg &
 sudo ./linuxptp/phc2sys -s eth0 -c CLOCK_REALTIME -w & # -f PTP2pcConfigQLANprojectSlave.cfg &
@@ -30,6 +31,7 @@ sudo config-pin P8_44 pruout
 sudo config-pin P8_45 pruout
 sudo config-pin P8_46 pruout
 sudo ./CppScripts/QtransportLayerAgentN client 192.168.8.2 192.168.8.1
+sudo systemctl enable systemd-timesyncd # start system synch
 sudo systemctl start systemd-timesyncd # start system synch
 sudo systemctl daemon-reload
 sudo timedatectl set-ntp true # Start NTP
