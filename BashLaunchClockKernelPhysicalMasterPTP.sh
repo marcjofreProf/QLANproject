030ef811609f96b57e11d2697136d53321aec988
marcjofre <marcjofre@Ubuntu-DESKTOP-JMF9ED3-MJ.myguest.virtualbox.org>
2024-04-29 20:53:32 +0200

    Testing synch 1

--- BashLaunchClockKernelPhysicalMasterPTP.sh
+++ BashLaunchClockKernelPhysicalMasterPTP.sh
@@ -6,20 +6,21 @@ trap "kill 0" EXIT
 echo 'Running PTP'
 
 sudo /etc/init.d/rsyslog stop # stop logging
-
-# If at least the grand master is synch to NTP (good reference)
-sudo systemctl enable systemd-timesyncd # start system synch
-sudo systemctl start systemd-timesyncd # start system synch
-sudo systemctl daemon-reload
-sudo timedatectl set-ntp true # Start NTP
-
-## If synch to the RTC of the system, stop the NTP
-#sudo timedatectl set-ntp false
-#sudo systemctl stop systemd-timesyncd # stop system synch
-#sudo systemctl disable systemd-timesyncd # start system synch
-
 sudo ./linuxptp/ptp4l -i eth0 & #-f PTP4lConfigQLANproject.cfg & #-m
-sudo ./linuxptp/phc2sys -s CLOCK_REALTIME -c eth0 -w & #-f PTP2pcConfigQLANprojectSlave.cfg & -m
+
+## If at least the grand master is synch to NTP (good reference)
+#sudo systemctl enable systemd-timesyncd # start system synch
+#sudo systemctl start systemd-timesyncd # start system synch
+#sudo systemctl daemon-reload
+#sudo timedatectl set-ntp true # Start NTP
+#sudo ./linuxptp/phc2sys -s CLOCK_REALTIME -c eth0 -w & #-f PTP2pcConfigQLANprojectSlave.cfg & -m
+
+## If synch to the RTC of the system, stop the NTP. The quality of the internal crystal/clock matters
+sudo timedatectl set-ntp false
+sudo systemctl stop systemd-timesyncd # stop system synch
+sudo systemctl disable systemd-timesyncd # start system synch
+sudo ./linuxptp/phc2sys -s eth0 -c CLOCK_REALTIME -w & #-f PTP2pcConfigQLANprojectSlave.cfg & -m
+
 echo 'Enabling PWM for 24 MHz ref clock'
 sudo config-pin P8.19 pwm
 sudo sh -c "echo '38' >> /sys/class/pwm/pwmchip7/pwm-7\:0/period"
