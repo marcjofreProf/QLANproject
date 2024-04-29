87fb848a2e34624388b6dc3ef135f0d95ec123bc
marcjofre <marcjofre@Ubuntu-DESKTOP-JMF9ED3-MJ.myguest.virtualbox.org>
2024-04-29 23:04:51 +0200

    Testing synch 6

--- CppScripts/BBBhw/GPIO.h
+++ CppScripts/BBBhw/GPIO.h
@@ -87,7 +87,7 @@ private:// Variables
 	    using rep        = duration::rep;
 	    using period     = duration::period;
 	    using time_point = std::chrono::time_point<my_clock>;
-	    static constexpr bool is_steady = false;// true, false
+	    static constexpr bool is_steady = true;// true, false
 
 	    static time_point now()
 	    {
