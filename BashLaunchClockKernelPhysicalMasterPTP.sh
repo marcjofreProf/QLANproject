e7a6ab739de9e66f0b158e52cfb8929603673e82
marcjofre <marcjofre@Ubuntu-DESKTOP-JMF9ED3-MJ.myguest.virtualbox.org>
2024-05-10 19:01:51 +0200

    Adjusting PTP 19

--- BashLaunchClockKernelPhysicalMasterPTP.sh
+++ BashLaunchClockKernelPhysicalMasterPTP.sh
@@ -6,20 +6,20 @@ trap "kill 0" EXIT
 echo 'Running PTP'
 
 sudo /etc/init.d/rsyslog stop # stop logging
-sudo ./linuxptp/ptp4l -i eth0 -f PTP4lConfigQLANproject.cfg & #-m
+sudo ./linuxptp/ptp4l -i eth0 -f PTP4lConfigQLANproject.cfg -m & #-m
 
 ## If at least the grand master is synch to NTP (good reference)
 sudo systemctl enable systemd-timesyncd # start system synch
 sudo systemctl start systemd-timesyncd # start system synch
 sudo systemctl daemon-reload
 sudo timedatectl set-ntp true # Start NTP
-sudo ./linuxptp/phc2sys -s CLOCK_REALTIME -c eth0 -w & #-f PTP2pcConfigQLANprojectSlave.cfg & -m
+sudo ./linuxptp/phc2sys -s CLOCK_REALTIME -c eth0 -w -m & #-f PTP2pcConfigQLANprojectSlave.cfg & -m
 
 ## If synch to the RTC of the system, stop the NTP. The quality of the internal crystal/clock matters
 #sudo timedatectl set-ntp false
 #sudo systemctl stop systemd-timesyncd # stop system synch
 #sudo systemctl disable systemd-timesyncd # start system synch
-#sudo ./linuxptp/phc2sys -s eth0 -c CLOCK_REALTIME -w & #-f PTP2pcConfigQLANprojectSlave.cfg & -m
+#sudo ./linuxptp/phc2sys -s eth0 -c CLOCK_REALTIME -w -m & #-f PTP2pcConfigQLANprojectSlave.cfg & -m
 
 sudo hwclock --systohc
 
