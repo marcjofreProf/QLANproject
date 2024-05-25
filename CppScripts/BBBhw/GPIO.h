dba132fa703019ff07606ef608bb45a0943c09de
marcjofre <marcjofre@Ubuntu-DESKTOP-JMF9ED3-MJ.myguest.virtualbox.org>
2024-05-25 22:17:47 +0200

    adj 32

--- CppScripts/BBBhw/GPIO.h
+++ CppScripts/BBBhw/GPIO.h
@@ -48,7 +48,7 @@ private:// Variables
 	std::atomic<bool> ManualSemaphoreExtra=false;
 	std::thread threadRefSynch; // Process thread that executes requests/petitions without blocking
 	long long int LostCounts=5; // For stoping and changing IEP counter. It has to do with jitter??? If not ajusted correctly, more jitter
-	int NumSynchMeasAvgAux=181;//31; // Num averages to compute the time error. Better to be odd number.
+	int NumSynchMeasAvgAux=15;//181;//31; // Num averages to compute the time error. Better to be odd number.
 	unsigned int NextSynchPRUcommand=4;// Do not do correction
 	unsigned int NextSynchPRUcorrection=0;// Correction or sequence signal value
 	double PRUoffsetDriftError=0;
