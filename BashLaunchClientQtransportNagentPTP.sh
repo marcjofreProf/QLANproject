0c77bc038da184883c5f7d96a0bb972b7bf51f1b
marcjofre <marcjofre@Ubuntu-DESKTOP-JMF9ED3-MJ.myguest.virtualbox.org>
2024-06-06 16:51:01 +0200

    Better PTP 2

--- BashLaunchClientQtransportNagentPTP.sh
+++ BashLaunchClientQtransportNagentPTP.sh
@@ -7,9 +7,9 @@ sudo pkill -f phc2sys
 sudo /etc/init.d/rsyslog stop # stop logging
 
 ## Get the current time in seconds and nanoseconds
-current_time=$(date +%s)
+#current_time=$(date +%s)
 #current_nano=$(date +%N)
-sudo phc_ctl /dev/ptp0 set $current_time # $current_nano # if the initial phc2sys offset is really huge. Then, run "sudo phc_ctl /dev/ptp0 set" before starting the ptp4l service, so that it has an initial time based on the RTC that is "in the ballpark" and and set "step_threshold" at least or below to 0.00002 in the config file so that it can jump to converge
+#sudo phc_ctl /dev/ptp0 set $current_time # $current_nano # if the initial phc2sys offset is really huge. Then, run "sudo phc_ctl /dev/ptp0 set" before starting the ptp4l service, so that it has an initial time based on the RTC that is "in the ballpark" and and set "step_threshold" at least or below to 0.00002 in the config file so that it can jump to converge
 sudo hwclock --systohc
 
 sudo timedatectl set-ntp false
