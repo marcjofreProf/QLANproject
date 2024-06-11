196da387959b01f033727cb851a8cb00db26ad56
marcjofre <marcjofre@Ubuntu-DESKTOP-JMF9ED3-MJ.myguest.virtualbox.org>
2024-06-11 16:28:20 +0200

    Better PTP 38

--- CppScripts/BBBhw/GPIO.cpp
+++ CppScripts/BBBhw/GPIO.cpp
@@ -353,7 +353,7 @@ int GPIO::PRUsignalTimerSynch(){
 		}		
 		this->requestWhileWait = this->SetWhileWait();// Used with non-busy wait
 		this->iIterPRUcurrentTimerVal++;
-		if (this->iIterPRUcurrentTimerValSynch==(3*this->ExtraNumSynchMeasAvgAux)){
+		if (this->iIterPRUcurrentTimerValSynch==(4*this->ExtraNumSynchMeasAvgAux)){
 			cout << "Synchronized, ready to proceed..." << endl;
 		}
 	}// end while
