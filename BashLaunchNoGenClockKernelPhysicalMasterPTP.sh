c59c816a2f5b839d3d8a1aee0a5459a705b44872
marcjofre <marcjofre@Ubuntu-DESKTOP-JMF9ED3-MJ.myguest.virtualbox.org>
2024-06-13 16:54:15 +0200

    Improving PTP 7

--- BashLaunchNoGenClockKernelPhysicalMasterPTP.sh
+++ BashLaunchNoGenClockKernelPhysicalMasterPTP.sh
@@ -31,14 +31,14 @@ sudo systemctl enable systemd-timesyncd # start system synch
 sudo systemctl start systemd-timesyncd # start system synch
 sudo systemctl daemon-reload
 sudo timedatectl set-ntp true # Start NTP
-sudo ./linuxptp/phc2sys -s CLOCK_REALTIME -c eth0 -w -m & #-f PTP2pcConfigQLANprojectSlave.cfg & -m
+sudo ./linuxptp/phc2sys -s CLOCK_REALTIME -c eth0 -w -f PTP4lConfigQLANproject.cfg -m & #-f PTP2pcConfigQLANprojectSlave.cfg & -m
 
 
 ## If synch to the RTC of the system, stop the NTP. The quality of the internal crystal/clock matters
 #sudo timedatectl set-ntp false
 #sudo systemctl stop systemd-timesyncd # stop system synch
 #sudo systemctl disable systemd-timesyncd # start system synch
-#sudo ./linuxptp/phc2sys -s eth0 -c CLOCK_REALTIME -w -m & #-f PTP2pcConfigQLANprojectSlave.cfg & -m
+#sudo ./linuxptp/phc2sys -s eth0 -c CLOCK_REALTIME -w -f PTP4lConfigQLANproject.cfg -m & #-f PTP2pcConfigQLANprojectSlave.cfg & -m
 
 sudo ./linuxptp/ptp4l -i eth0 -f PTP4lConfigQLANproject.cfg -m #& #-m
 
