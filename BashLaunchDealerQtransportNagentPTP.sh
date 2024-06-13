6ec80cb231ccd9010dc559dedb582d4edd63b4bf
marcjofre <marcjofre@Ubuntu-DESKTOP-JMF9ED3-MJ.myguest.virtualbox.org>
2024-06-13 19:44:48 +0200

    Better PTP 1

--- BashLaunchDealerQtransportNagentPTP.sh
+++ BashLaunchDealerQtransportNagentPTP.sh
@@ -14,7 +14,7 @@ sudo hwclock --systohc
 # Configure SYSTEM CLOCKS: CLOCK_REALTIME and CLOCK_TAI
 sudo pmc -u -b 0 -t 1 "SET GRANDMASTER_SETTINGS_NP clockClass 248 \
         clockAccuracy 0xfe offsetScaledLogVariance 0xffff \
-        currentUtcOffset 0 leap61 0 leap59 0 currentUtcOffsetValid 1 \
+        currentUtcOffset 37 leap61 0 leap59 0 currentUtcOffsetValid 1 \
         ptpTimescale 1 timeTraceable 1 frequencyTraceable 0 \
         timeSource 0xa0"
 
