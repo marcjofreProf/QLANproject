e0e8336d8369b16a6a0087eaa2a3b3e185183145
marcjofre <marcjofre@Ubuntu-DESKTOP-JMF9ED3-MJ.myguest.virtualbox.org>
2024-06-15 21:32:06 +0200

    Better PTP 46

--- BashLaunchClockKernelPhysicalSlavePTP.sh
+++ BashLaunchClockKernelPhysicalSlavePTP.sh
@@ -32,12 +32,12 @@ sudo systemctl disable systemd-timesyncd # disable system synch
 #sudo adjtimex --print # Print something to make sure that adjtimex is installed (sudo apt-get update; sudo apt-get install adjtimex
 #sudo adjtimex ...# manually make sure to adjust the conversion from utc to tai and viceversa
 sudo ./linuxptp/ptp4l -i eth0 -s -H -f PTP4lConfigQLANprojectSlave.cfg -m & #-m
-pidAux=$(pidof -s ptp4l)
-sudo chrt -f -p 0 $pidAux
+pidAux=$(pgrep -f "^ptp4l$")
+sudo chrt -f -p 0 "$pidAux"
 
 sudo ./linuxptp/phc2sys -s eth0 -c CLOCK_REALTIME -w -f PTP4lConfigQLANprojectSlave.cfg & # -w -f PTP2pcConfigQLANprojectSlave.cfg & # -m # Important to launch phc2sys first (not in slave)
-pidAux=$(pidof -s phc2sys)
-sudo chrt -f -p 0 $pidAux
+pidAux=$(pgrep -f "^phc2sys$")
+sudo chrt -f -p 0 "$pidAux"
 
 echo 'Enabling PWM for 24 MHz ref clock'
 sudo config-pin P8.19 pwm
@@ -79,8 +79,8 @@ sudo config-pin P8_44 pruout
 sudo config-pin P8_45 pruout
 sudo config-pin P8_46 pruout
 sudo ./BBBclockKernelPhysical/BBBclockKernelPhysicalDaemon $1 $2 $3
-pidAux=$(pidof -s BBBclockKernelPhysicalDaemon)
-sudo chrt -f -p 0 $pidAux
+pidAux=$(pgrep -f "^BBBclockKernelPhysicalDaemon$")
+sudo chrt -f -p 0 "$pidAux"
 
 sudo systemctl enable --now systemd-timesyncd # enable system synch
 sudo systemctl start systemd-timesyncd # start system synch
