ae3c5bc6848ff06323ce1d596edb1e7e309234f8
marcjofre <marcjofre@Ubuntu-DESKTOP-JMF9ED3-MJ.myguest.virtualbox.org>
2024-06-19 18:38:07 +0200

    Better launch scripts 2

--- BashLaunchClockKernelPhysicalFreeRun.sh
+++ BashLaunchClockKernelPhysicalFreeRun.sh
@@ -3,7 +3,7 @@
 # arg1: Daemon ticks to fine adust to required Frequency: For example, 0.0 for 1pps, or -99900000 for 1 KHz. Defined double, but it has to be small in order to not produce negative half periods (defined as unsigned int)
 # arg2: Daemon average filter, median filter or mean window. For example (odd number): 5. 1<= arg2 <= 50. Probably, only larger if there is too much jitter.
 # arg3: Daemon print PID values: true or false
-trap "kill 0" EXIT
+
 echo 'Free Running'
 # Kill non-wanted processes
 sudo pkill -f nodejs # javascript applications
@@ -74,19 +74,19 @@ sudo nice -n -20 ./BBBclockKernelPhysical/BBBclockKernelPhysicalDaemon $1 $2 $3 
 pidAux=$(pgrep -f "BBBclockKernelPhysicalDaemon")
 sudo chrt -f -p 1 $pidAux
 
-read -r -p "Press return to stop all processes" # Block operation until Ctrl+C is pressed
+read -r -p "Press Ctrl+C to kill all processes" # Block operation until Ctrl+C is pressed
 
-EXIT:
-	sudo sh -c "echo '0' >> /sys/class/pwm/pwmchip7/pwm-7\:0/enable"
-	sudo sh -c "echo '0' >> /sys/class/pwm/pwmchip4/pwm-4\:0/enable"
-	sudo sh -c "echo '0' >> /sys/class/pwm/pwmchip1/pwm-1\:0/enable" 
+sudo sh -c "echo '0' >> /sys/class/pwm/pwmchip7/pwm-7\:0/enable"
+sudo sh -c "echo '0' >> /sys/class/pwm/pwmchip4/pwm-4\:0/enable"
+sudo sh -c "echo '0' >> /sys/class/pwm/pwmchip1/pwm-1\:0/enable" 
 
-	sudo systemctl enable --now systemd-timesyncd # enable system synch
-	sudo systemctl start systemd-timesyncd # start system synch
-	sudo systemctl daemon-reload
-	sudo timedatectl set-ntp true # Start NTP
-	sudo hwclock --systohc
-	echo 'Stopped PTP'
+sudo systemctl enable --now systemd-timesyncd # enable system synch
+sudo systemctl start systemd-timesyncd # start system synch
+sudo systemctl daemon-reload
+sudo timedatectl set-ntp true # Start NTP
+sudo hwclock --systohc
+echo 'Stopped PTP'
+trap "kill 0" EXIT
 #sudo /etc/init.d/rsyslog start # start logging
 # Kill all the launched processes with same group PID
 #kill -INT $$
