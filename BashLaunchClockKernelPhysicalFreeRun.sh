9b3681707192e4d15040a5b96a86808593fd4ad7
marcjofre <marcjofre@Ubuntu-DESKTOP-JMF9ED3-MJ.myguest.virtualbox.org>
2024-06-19 18:45:23 +0200

    Better launch scripts 4

--- BashLaunchClockKernelPhysicalFreeRun.sh
+++ BashLaunchClockKernelPhysicalFreeRun.sh
@@ -3,6 +3,7 @@
 # arg1: Daemon ticks to fine adust to required Frequency: For example, 0.0 for 1pps, or -99900000 for 1 KHz. Defined double, but it has to be small in order to not produce negative half periods (defined as unsigned int)
 # arg2: Daemon average filter, median filter or mean window. For example (odd number): 5. 1<= arg2 <= 50. Probably, only larger if there is too much jitter.
 # arg3: Daemon print PID values: true or false
+trap cleanup_on_SIGINT SIGINT
 trap "kill 0" EXIT
 echo 'Free Running'
 # Kill non-wanted processes
