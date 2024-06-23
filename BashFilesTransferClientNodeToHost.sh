4fc731e1508a2199f0fbdf357160837ef1154e6b
marcjofre <marcjofre@Ubuntu-DESKTOP-JMF9ED3-MJ.myguest.virtualbox.org>
2024-06-23 17:47:59 +0200

    Jitterless 10

--- BashFilesTransferClientNodeToHost.sh
+++ BashFilesTransferClientNodeToHost.sh
@@ -3,7 +3,7 @@
 # arg1: to specify if NTP or PTP or others
 sudo scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null /home/debian/Scripts/QLANproject/PRUdata/TimetaggingData marcjofre@10.0.0.1:/home/marcjofre/Scripts/QLANproject/PRUdata/ClientRawStoredQubitsNode$1
 
-sudo scp -r -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null /home/debian/Scripts/OSrealtimeTools/rt-tests/LatencyHistData/ marcjofre@10.0.0.1:/home/marcjofre/Scripts/OSrealtimeTools/rt-tests/LatencyHistData/
+sudo scp -r -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null /home/debian/Scripts/OSrealtimeTools/rt-tests/LatencyHistData/ marcjofre@10.0.0.1:/home/marcjofre/Scripts/OSrealtimeTools/rt-tests/
 
 echo 'Sent files from client node to host'
 # Typically, if a message like ssh: connect to host 10.0.0.1 port 22: Connection refused.lost connection.
