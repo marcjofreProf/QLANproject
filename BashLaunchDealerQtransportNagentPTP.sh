e177fe44941dc7b557fde75ea1453b1c6b53e554
marcjofre <marcjofre@Ubuntu-DESKTOP-JMF9ED3-MJ.myguest.virtualbox.org>
2024-06-28 10:08:17 +0200

    Better TTU 1

--- BashLaunchDealerQtransportNagentPTP.sh
+++ BashLaunchDealerQtransportNagentPTP.sh
@@ -88,5 +88,6 @@ sudo nice -n -20 ./CppScripts/QtransportLayerAgentN dealer 192.168.10.2 192.168.
 pidAux=$(pgrep -f "QtransportLayerAgentN")
 sudo chrt -f -p 1 $pidAux
 
-read -r -p "Press Ctrl+C to kill launched processes\n" # Block operation until Ctrl+C is pressed
+read -r -p "Press Ctrl+C to kill launched processes
+" # Block operation until Ctrl+C is pressed
 
