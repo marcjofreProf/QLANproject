71e07f5e5d72227808618a3a2b3b0a9cbec963ae
marcjofre <marcjofre@Ubuntu-DESKTOP-JMF9ED3-MJ.myguest.virtualbox.org>
2024-07-21 14:50:58 +0200

    Fine tunning 34

--- CppScripts/QphysLayerAgent.cpp
+++ CppScripts/QphysLayerAgent.cpp
@@ -32,7 +32,7 @@ Agent script for Quantum Physical Layer
 #include <atomic>
 // time points
 #define WaitTimeToFutureTimePoint 399000000 // Max 999999999. It is the time barrier to try to achieve synchronization. Considered nanoseconds (it can be changed on the transformatoin used)
-#define UTCoffsetBarrierErrorThreshold 37000000000 // Some BBB when synch with linuxPTP have an error on the UTC offset with respect TAI. Remove this sistemic offset and announce it!
+#define UTCoffsetBarrierErrorThreshold 400000000 //37000000000 // Some BBB when synch with linuxPTP have an error on the UTC offset with respect TAI. Remove this sistemic offset and announce it!
 // Mathematical calculations
 #include <cmath>
 
