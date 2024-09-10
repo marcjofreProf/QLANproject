2511a8a2f81601b4fe5cb21064343d4afceff5e0
marcjofre <marcjofre@Ubuntu-DESKTOP-JMF9ED3-MJ.myguest.virtualbox.org>
2024-09-10 15:07:15 +0200

    Specific chan 2

--- CppScripts/BashMakeFileQtransportLayerAgentPython.sh
+++ CppScripts/BashMakeFileQtransportLayerAgentPython.sh
@@ -1,3 +1,4 @@
+# The Python folder in /usr/include/pythonX.XX (and specifically the file Python.h) has to exist. Actually, it is good to copy the compressed folder python3.10 to the location /usr/include/ as "sudo tar -xvzf python3_10.tar.gz -C /usr/include/python3.10 (first create python3.10 directory "sudo mkdir python3.10")
 swig -c++ -python QtransportLayerAgent.i
 gcc -O2 -fPIC -c QtransportLayerAgentH.cpp -I/usr/local/lib/python3.10/dist-packages/numpy/core/include
 gcc -O2 -fPIC -c QtransportLayerAgent_wrap.cxx -I/usr/include/python3.10 -I/usr/local/lib/python3.10/dist-packages/numpy/core/include
