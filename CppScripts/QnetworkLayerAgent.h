6b8928babd0f9e1e2b4ec0d9127e921b8a91a89f
marcjofre <marcjofre@Ubuntu-DESKTOP-JMF9ED3-MJ.myguest.virtualbox.org>
2024-10-06 19:21:02 +0200

    Eff Period 42

--- CppScripts/QnetworkLayerAgent.h
+++ CppScripts/QnetworkLayerAgent.h
@@ -35,7 +35,7 @@ class QNLA {
 private:// Variables/Instances		
 	int numberHops=0;
 	// Semaphore
-	unsigned long long int UnTrapSemaphoreValueMaxCounter=450000000000;//MAx counter trying to acquire semaphore, then force release
+	unsigned long long int UnTrapSemaphoreValueMaxCounter=450000000;//MAx counter trying to acquire semaphore, then force release
 	std::atomic<bool> valueSemaphore=true;// Start as 1 (open or acquireable)
 	// Payload messages
 	char PayloadReadBuffer[NumBytesPayloadBuffer]={0}; //Buffer to read payload messages
