e03c36b8bdf46730b8a45f86407f98adbf76c924
marcjofre <marcjofre@Ubuntu-DESKTOP-JMF9ED3-MJ.myguest.virtualbox.org>
2024-10-10 18:01:05 +0200

    Slid 43

--- CppScripts/BBBhw/GPIO.cpp
+++ CppScripts/BBBhw/GPIO.cpp
@@ -537,8 +537,8 @@ int GPIO::ReadTimeStamps(int iIterRunsAux,int QuadEmitDetecSelecAux, double Sync
 	pru0dataMem_int[2]=static_cast<unsigned int>(this->SynchTrigPeriod);// Indicate period of the sequence signal, so that it falls correctly and it is picked up by the Signal PRU. Link between system clock and PRU clock. It has to be a power of 2
 	pru0dataMem_int[1]=static_cast<unsigned int>(this->NumQuBitsPerRun); // set number captures
 	// The Absolute error is introduced at each signal trigger and timetagging sequence
-	ldTimePointClockTagPRUinitial=static_cast<long double>(std::chrono::duration_cast<std::chrono::nanoseconds>(this->QPLAFutureTimePoint.time_since_epoch()).count())/static_cast<long double>(PRUclockStepPeriodNanoseconds);// update value
-	long double ldPRUoffsetDriftErrorAvg=(ldTimePointClockTagPRUinitial+static_cast<long double>(PRUoffsetDriftErrorAbsAvgMax))*static_cast<long double>(PRUoffsetDriftErrorAvg)*static_cast<long double>(1000000000.0)/static_cast<long double>(PRUclockStepPeriodNanoseconds);
+	//ldTimePointClockTagPRUinitial=static_cast<long double>(std::chrono::duration_cast<std::chrono::nanoseconds>(this->QPLAFutureTimePoint.time_since_epoch()).count())/static_cast<long double>(PRUclockStepPeriodNanoseconds);// update value
+	long double ldPRUoffsetDriftErrorAvg=(static_cast<long double>(PRUoffsetDriftErrorAbsAvgMax))*static_cast<long double>(PRUoffsetDriftErrorAvg)*static_cast<long double>(1000000000.0)/static_cast<long double>(PRUclockStepPeriodNanoseconds);
 	double dPRUoffsetDriftErrorAvg=0.0;
 	if (ldPRUoffsetDriftErrorAvg<0.0){
 		dPRUoffsetDriftErrorAvg=static_cast<double>(-fmodl(-ldPRUoffsetDriftErrorAvg,static_cast<long double>(MultFactorEffSynchPeriod*SynchTrigPeriod)));
@@ -667,8 +667,8 @@ int GPIO::SendTriggerSignals(int QuadEmitDetecSelecAux, double SynchTrigPeriodAu
 	pru1dataMem_int[3]=static_cast<unsigned int>(this->SynchTrigPeriod);// Indicate period of the sequence signal, so that it falls correctly and is picked up by the Signal PRU. Link between system clock and PRU clock. It has to be a power of 2
 	pru1dataMem_int[1]=static_cast<unsigned int>(this->NumberRepetitionsSignal); // set the number of repetitions
 	// The Absolute error is introduced at each signal trigger and timetagging sequence
-	ldTimePointClockTagPRUinitial=static_cast<long double>(std::chrono::duration_cast<std::chrono::nanoseconds>((this->QPLAFutureTimePoint).time_since_epoch()).count())/static_cast<long double>(PRUclockStepPeriodNanoseconds);// update value
-	long double ldPRUoffsetDriftErrorAvg=(ldTimePointClockTagPRUinitial+static_cast<long double>(PRUoffsetDriftErrorAbsAvgMax))*static_cast<long double>(PRUoffsetDriftErrorAvg)*static_cast<long double>(1000000000.0)/static_cast<long double>(PRUclockStepPeriodNanoseconds);
+	//ldTimePointClockTagPRUinitial=static_cast<long double>(std::chrono::duration_cast<std::chrono::nanoseconds>((this->QPLAFutureTimePoint).time_since_epoch()).count())/static_cast<long double>(PRUclockStepPeriodNanoseconds);// update value
+	long double ldPRUoffsetDriftErrorAvg=(static_cast<long double>(PRUoffsetDriftErrorAbsAvgMax))*static_cast<long double>(PRUoffsetDriftErrorAvg)*static_cast<long double>(1000000000.0)/static_cast<long double>(PRUclockStepPeriodNanoseconds);
 	double dPRUoffsetDriftErrorAvg=0.0;
 	if (ldPRUoffsetDriftErrorAvg<0.0){
 		dPRUoffsetDriftErrorAvg=static_cast<double>(-fmodl(-ldPRUoffsetDriftErrorAvg,static_cast<long double>(MultFactorEffSynchPeriod*SynchTrigPeriod)));
@@ -681,7 +681,7 @@ int GPIO::SendTriggerSignals(int QuadEmitDetecSelecAux, double SynchTrigPeriodAu
 		PRUoffsetDriftErrorAbsAvgAux=-fmod(-PRUoffsetDriftErrorAbsAvgAux,MultFactorEffSynchPeriod*SynchTrigPeriod);
 	}
 	else{
-		PRUoffsetDriftErrorAbsAvgAux=+fmod(PRUoffsetDriftErrorAbsAvgAux,MultFactorEffSynchPeriod*SynchTrigPeriod);
+		PRUoffsetDriftErrorAbsAvgAux=fmod(PRUoffsetDriftErrorAbsAvgAux,MultFactorEffSynchPeriod*SynchTrigPeriod);
 	}
 	if (abs(PRUoffsetDriftErrorAbsAvgAux)>PRUoffsetDriftErrorAbsAvgMax){
 		cout << "GPIO::PRUoffsetDriftErrorAbsAvgAux magnitude is too large: " << PRUoffsetDriftErrorAbsAvgAux << "Increase PRUoffsetDriftErrorAbsAvgMax. Wrapping PRUoffsetDriftErrorAbsAvgAux around PRUoffsetDriftErrorAbsAvgMax." << endl;
