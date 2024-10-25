a634bac3945f0e8f488315f6a70a4146b9b70a49
marcjofre <marcjofre@Ubuntu-DESKTOP-JMF9ED3-MJ.myguest.virtualbox.org>
2024-10-25 10:04:07 +0200

    Tunning PTP 77

--- CppScripts/BBBhw/GPIO.h
+++ CppScripts/BBBhw/GPIO.h
@@ -55,8 +55,8 @@ private:// Variables
 	std::thread threadRefSynch; // Process thread that executes requests/petitions without blocking
 	long long int LostCounts=4; // For stoping and changing IEP counter. It has to do with jitter??? If not ajusted correctly, more jitter
 	int ApproxInterruptTime=10000; // Typical time of interrupt time duration is 5000 with simple busy wait; around 8000 with busy wait with yield
-	int NumSynchMeasAvgAux=21;//51; // Num averages to compute the relative frequency difference. Better to be odd number.
-	int ExtraNumSynchMeasAvgAux=21; // Averaging for computing current absolute time offset
+	int NumSynchMeasAvgAux=51;//51; // Num averages to compute the relative frequency difference. Better to be odd number.
+	int ExtraNumSynchMeasAvgAux=41; // Averaging for computing current absolute time offset
 	unsigned int NextSynchPRUcommand=11;// set initially to NextSynchPRUcorrection=0
 	unsigned int NextSynchPRUcorrection=0;// Correction or sequence signal value
 	//unsigned int OffsetSynchPRUBaseCorrection=262144;// Base value from where the synch offset is added or discounted to achieve periodic offset correction
