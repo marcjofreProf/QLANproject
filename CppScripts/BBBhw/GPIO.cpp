b52ca31d85dae7fc49c07fba219ac380e308efdf
marcjofre <marcjofre@Ubuntu-DESKTOP-JMF9ED3-MJ.myguest.virtualbox.org>
2024-11-13 15:09:48 +0100

    Tunning 17

--- CppScripts/BBBhw/GPIO.cpp
+++ CppScripts/BBBhw/GPIO.cpp
@@ -338,7 +338,7 @@ int GPIO::PRUsignalTimerSynchJitterLessInterrupt(){
 	this->NextSynchPRUcommand=static_cast<unsigned int>(11); // set command 11, do absolute correction
 	while(true){		
 		clock_nanosleep(CLOCK_REALTIME,TIMER_ABSTIME,&requestWhileWait,NULL);
-		if (this->ManualSemaphoreExtra==false and Clock::now()<this->TimePointClockCurrentSynchPRU1future){// It was possible to execute when needed, and still on time to be executed (otherwise skip it to not produce accumulations)			
+		if (this->ManualSemaphoreExtra==false and Clock::now()<(this->TimePointClockCurrentSynchPRU1future-std::chrono::nanoseconds(2*TimePRUcommandDelay))){// It was possible to execute when needed, and still on time to be executed (otherwise skip it to not produce accumulations)			
 			//cout << "Resetting PRUs timer!" << endl;
 			//if (clock_nanosleep(CLOCK_REALTIME,TIMER_ABSTIME,&requestWhileWait,NULL)==0 and this->ManualSemaphore==false and this->ResetPeriodicallyTimerPRU1){// Synch barrier. CLOCK_TAI (with steady_clock) instead of CLOCK_REALTIME (with system_clock).//https://opensource.com/article/17/6/timekeeping-linux-vms
 			if (this->ResetPeriodicallyTimerPRU1){
