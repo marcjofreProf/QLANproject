d609910511fc1f5f39cbc5c63589e082e1753329
marcjofre <marcjofre@Ubuntu-DESKTOP-JMF9ED3-MJ.myguest.virtualbox.org>
2025-10-18 16:08:26 +0000

    Improvements 40

--- CppScripts/QphysLayerAgent.cpp
+++ CppScripts/QphysLayerAgent.cpp
@@ -1512,11 +1512,12 @@ if (SimulateNumStoredQubitsNodeAux>1){
 	CoincidenceWindowPRU=static_cast<long long int>(0.5*HistPeriodicityAux);
 
 	// Full coincidence in time and channel
+	// since synchronizaton with periodic pattern can turn into one channel adjusting to the lower side and the other to the higher side (in terms of time). Hence, watch out!
 	for (int i = 0; i < (QuadNumChGroups-1); i++) {
       for (int j = i+1; j < (QuadNumChGroups); j++) {
           for (unsigned int k = 0; k < RawTotalCurrentNumRecordsQuadCh[i]; k++){
             	for (unsigned int l = 0; l < RawTotalCurrentNumRecordsQuadCh[j]; l++){
-	                if ((abs(static_cast<long long int>(TimeTaggs[i][k]) - static_cast<long long int>(TimeTaggs[j][l]))<CoincidenceWindowPRU or (abs(static_cast<long long int>(TimeTaggs[i][k]) - static_cast<long long int>(TimeTaggs[j][l])+static_cast<long long int>(HistPeriodicityAux*MultFactorEffSynchPeriodQPLA))<CoincidenceWindowPRU) or (abs(static_cast<long long int>(TimeTaggs[i][k]) - static_cast<long long int>(TimeTaggs[j][l])-+static_cast<long long int>(HistPeriodicityAux*MultFactorEffSynchPeriodQPLA))<CoincidenceWindowPRU)) and TimeTaggs[i][k]!=0 and TimeTaggs[j][l]!=0){//} and ((BitPositionChannelTags(ChannelTags[i][k]))==(BitPositionChannelTags(ChannelTags[j][l])))){
+	                if (abs(static_cast<long long int>(TimeTaggs[i][k]) - static_cast<long long int>(TimeTaggs[j][l]))<CoincidenceWindowPRU and TimeTaggs[i][k]!=0 and TimeTaggs[j][l]!=0){// and ((BitPositionChannelTags(ChannelTags[i][k]))==(BitPositionChannelTags(ChannelTags[j][l])))){
 	                    TimeTaggsDetAnalytics[4]+=1.0;; // Repetition found
 	                }
               }
